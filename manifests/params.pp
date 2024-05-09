0f8337559c6f15684f9f5358a6e0d86113ec2c7b
SurDaft - Jack Stupple <surdaft@users.noreply.github.com>
2024-05-09 11:22:40 +0100

    Update moved nvm repo and default version

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,8 +3,8 @@ class nvm::params {
   $manage_user         = false
   $manage_dependencies = true
   $manage_profile      = true
-  $version             = 'v0.29.0'
-  $nvm_repo            = 'https://github.com/creationix/nvm.git'
+  $version             = 'v0.39.7'
+  $nvm_repo            = 'https://github.com/nvm-sh/nvm.git'
   $refetch             = false
   $install_node        = undef
   $node_instances      = {}
